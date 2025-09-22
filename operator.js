22a56d4d33d0c225f26f1e997d5b3c7a69a99f6b
Nasiruddin2121 <nasiruddin2121@gmail.com>
2025-09-22 23:59:04 +0600

    operator.js

---
+++ operator.js
@@ -0,0 +1,39 @@
+//Arithmetic operator
+// + - * / % ++ --
+var a =11
+var b=6
+console.log(a%b)
+
+//Incremental
+    //Pre Incremental, Post Incremental
+
+console.log(++a)
+console.log(a)
+
+// Assignment operator
+var a =10
+var b=20
+a +=b
+console.log(a)
+a-=b
+console.log(a)
+a *=b
+console.log(a)
+a /=b
+console.log(a)
+a %=b
+console.log(a)
+
+//Comparison Operation
+var a =10
+var b=20
+
+console.log(a==b)
+console.log(a!=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a===b)
+console.log(a!==b)
+
