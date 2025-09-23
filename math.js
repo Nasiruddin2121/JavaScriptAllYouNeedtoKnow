0d56e8828bd90305a67b2f1b73f42a423e01bfbe
Nasiruddin2121 <nasiruddin2121@gmail.com>
2025-09-24 01:05:58 +0600

    math.js

---
+++ math.js
@@ -0,0 +1,18 @@
+console.log(Math.E)
+
+console.log(Math.PI)
+
+var n =4.483
+
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+console.log(Math.max(400,600,500))
+console.log(Math.min(300,200,400))
+console.log(Math.pow(2,3))
+console.log(Math.pow(5,2))
+console.log(Math.sqrt(9))
+console.log(Math.sqrt(64))
+
+console.log(Math.round(Math.random()*50+1))
